9ddb063d19f77f3a45a3ea89b8d8ef09aeaa0fdc
Futeki <Povizun@bk.ru>
2024-06-02 18:36:14 +0300

    registered methods in admin panel, added csu command, upgraded serializer for course to output lessons, added fixtures

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,8 +3,15 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from lms.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(ModelSerializer):
     lessons_in_course = SerializerMethodField()
+    lessons = LessonSerializer(many=True, source='lesson')
 
     class Meta:
         model = Course
@@ -13,8 +20,3 @@ class CourseSerializer(ModelSerializer):
     def get_lessons_in_course(self, instance):
         return instance.lesson.all().count()
 
-
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
