041a1143b8851ac788427f28cbb54a906e025748
Futeki <Povizun@bk.ru>
2024-06-19 12:28:09 +0300

    Added check for send_mail_about_course_update

--- lms/tasks.py
+++ lms/tasks.py
@@ -11,12 +11,13 @@ from lms.models import Subscription
 def send_mail_about_course_update(course):
     """Отправка письма об обновлении курса подписанным пользователям"""
     subscriptions = Subscription.objects.filter(course=course.pk)
-    try:
-        send_mail(
-            "Курс, на который вы подписаны, обновился",
-            f"Курс {course.title} обновился",
-            EMAIL_HOST_USER,
-            [subscription.user.email for subscription in subscriptions],
-        )
-    except smtplib.SMTPException as e:
-        print(f"Ошибка отправки рассылки: {e}")
+    if subscriptions:
+        try:
+            send_mail(
+                "Курс, на который вы подписаны, обновился",
+                f"Курс {course.title} обновился",
+                EMAIL_HOST_USER,
+                [subscription.user.email for subscription in subscriptions],
+            )
+        except smtplib.SMTPException as e:
+            print(f"Ошибка отправки рассылки: {e}")
